5286d506345887ad0230b19a9382582b57e51fe3
Jesús <jrodriguez@axonico.net>
2023-01-09 15:29:29 -0300

    Usequery

--- src/Components/SearchPokemon/index.jsx
+++ src/Components/SearchPokemon/index.jsx
@@ -6,20 +6,38 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import CircularProgress from '@mui/material/CircularProgress'
 import CardPokemon from "../Card";
 import { useSelector, useDispatch } from 'react-redux'
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 import axios from 'axios'
 import { ReactQueryDevtools} from 'react-query/devtools'
 
 
-const SearchPokemon = ()=>{
-  const {data, isloading,error, HandleSubmit} = useAxios()
+const SearchPokemon = ()=>{  
+  // const {data, isloading,error, HandleSubmit} = useAxios()
   const [pokemon, setPokemon] = useState("")
+  
+  const fetchPokemon = async () =>{    
+    let selectedPokemon = pokemon.toLowerCase().trim()
+    const {data} = await  axios.get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`)
+    setPokemon("")
+    return data
+  }
+  const { isLoading, error, isError, data, refetch, isFetching, status } = useQuery('repoData', ()=>fetchPokemon(),{
+    refetchOnWindowFocus: false,
+    enabled: false ,
+    keepPreviousData: true,
+    refetchOnMount : false,
+    //refetchInterval : false,
+    retry : 0    
+    })  
+
+  
+
   
   return <>
     <Box component="form" noValidate onSubmit={(e)=>{
-      //refetch()            
+      refetch()            
       e.preventDefault()  
-      HandleSubmit(e, pokemon)
+      //HandleSubmit(e, pokemon)
       setPokemon("")  
       
     }}>      
@@ -27,15 +45,17 @@ const SearchPokemon = ()=>{
         <OutlinedInput placeholder="Busca tú pokemon favorito" value={pokemon} onChange={(e)=>{setPokemon(e.target.value)}} />
       </FormControl>
     </Box> 
+    {console.log(status)}   
+    
     {/* Con useAxios */}    
-    {isloading && <CircularProgress/>  }
+    {/* {isloading && <CircularProgress/>  }
     {error ? <h2>{error}</h2> : ''} 
-    {data ? <CardPokemon {...data}/> : ''}
+    {data ? <CardPokemon {...data}/> : ''} */}
 
     {/* Con useQuery */}
-    {/* {isFetching && <CircularProgress/>  }
-    {isError ? <h2>{error.response.data}</h2> : ''} */}
-    {/* {isLoading ? <CardPokemon {...data}/> : ''} */}
+    {status == 'loading' && <CircularProgress/>  }
+    {status == 'error' ? <h2>{error.response.data}</h2> : ''} 
+    {status == 'success' ? <CardPokemon {...data}/> : ''}
     
     <ReactQueryDevtools/>
   </>
